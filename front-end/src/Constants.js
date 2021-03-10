b03ba5e4b7080efc4cdcd721e435440715dd75f0
Vanessa Chan <chw.vanessa@gmail.com>
2021-03-09 20:30:28 -0500

    Global styles and constants

---
+++ front-end/src/Constants.js
@@ -0,0 +1,10 @@
+export const COLORS = {
+  background: "#ECF0F8",
+  overlay1: "#FFFFFF",
+  text1: "#black",
+  text2: "white",
+  icon1: "#787ab8",
+  mainBlue: "#567BF3",
+  gradientRoseanna: "linear-gradient(to left, #ffc3a0, #ffafbd)",
+  gradientTranquil: "linear-gradient(to left, #eecda3, #ef629f)",
+};
