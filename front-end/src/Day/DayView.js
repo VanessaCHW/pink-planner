574b88a8e9b87cd19b7734cb3242feea3bb69d26
Vanessa Chan <chw.vanessa@gmail.com>
2021-03-16 23:53:55 -0400

    Added DayViw Component

---
+++ front-end/src/Day/DayView.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import { COLORS } from "../Constants";
+
+const DayView = () => {
+  return (
+    <div>
+      <div>Some date data</div>
+    </div>
+  );
+};
+
+export default DayView;
