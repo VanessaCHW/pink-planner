d53a0d5cfa99caf98665bbfd508df67463828efd
Vanessa Chan <chw.vanessa@gmail.com>
2021-03-17 22:31:09 -0400

    End date, time (not copied in form), sections

--- front-end/src/MeetingForm.js
+++ front-end/src/MeetingForm.js
@@ -5,8 +5,45 @@ import Calendar from "react-calendar";
 import { format } from "date-fns";
 import { FiCalendar } from "react-icons/fi";
 import { BsArrowRight } from "react-icons/bs";
+import { GrLocation } from "react-icons/gr";
+import { RiNotification2Line } from "react-icons/ri";
+import { useHistory } from "react-router";
+import { v4 as uuidv4 } from "uuid";
+import DateIcons from "./Homepage/DateIcon";
+
+let initialEvent = {
+  kind: "calendar-event",
+  id: null,
+  title: null,
+  description: null,
+  location: null,
+  creator: {
+    name: "Vanessa Chan",
+  },
+  start: {
+    date: null,
+    time: null,
+  },
+  end: {
+    date: null,
+    time: null,
+  },
+  reminders: [
+    {
+      method: null,
+      minutes: null,
+    },
+  ],
+};
 
 const MeetingForm = () => {
+  const history = useHistory();
+  const [form, setForm] = useState(initialEvent);
+
+  const getNewID = () => {
+    return uuidv4();
+  };
+
   /******************************* */
   /** Select start/end date Input fields */
   /******************************* */
@@ -15,48 +52,86 @@ const MeetingForm = () => {
   const [displayStartDate, setDisplayStartDate] = useState("");
   const [displayEndDate, setDisplayEndDate] = useState("");
 
-  const handleSelectDateField = () => {
-    document.getElementById("CalendarForm").style.visibility = "visible";
+  const startField = () => {
+    document.getElementById("CalendarFormStart").style.visibility = "visible";
+  };
+  const endField = () => {
+    document.getElementById("CalendarFormEnd").style.visibility = "visible";
   };
 
   /******************************* */
   /**CALENDAR STATES AND FUNCTIONS */
   /******************************* */
   const [CalendarStartDate, setCalendarStartDate] = useState(new Date());
+  const [CalendarEndDate, setCalendarEndDate] = useState(new Date());
 
-  function onCalendarChange(nextValue) {
-    setCalendarStartDate(nextValue);
-  }
+  const onStartCalendarChange = (nextValue) => setCalendarStartDate(nextValue);
+  const onEndCalendarChange = (nextValue) => setCalendarEndDate(nextValue);
 
-  //Selecting date in calendar view
-  const handleClickDate = (value) => {
-    setCalendarStartDate(value);
-  };
+  const selectStartDate = (value) => setCalendarStartDate(value); //Selecting date in calendar view
+  const selectEndDate = (value) => setCalendarEndDate(value); //Selecting date in calendar view
 
   // OK button in the calendar view
-  const handleSubmitDate = (event) => {
+  const submitStartDate = (event) => {
     event.preventDefault();
-    document.getElementById("CalendarForm").style.visibility = "hidden";
+    document.getElementById("CalendarFormStart").style.visibility = "hidden";
     setEventStartDate(CalendarStartDate);
+
     let formatted = format(CalendarStartDate, "EEE. MMM. d, y");
     setDisplayStartDate(formatted);
+
+    if (eventEndDate === "" || eventEndDate < CalendarStartDate) {
+      setEventEndDate(CalendarStartDate);
+      let formatted = format(CalendarStartDate, "EEE. MMM. d, y");
+      setDisplayEndDate(formatted);
+    }
+  };
+  const submitEndDate = (event) => {
+    event.preventDefault();
+    document.getElementById("CalendarFormEnd").style.visibility = "hidden";
+    setEventEndDate(CalendarEndDate);
+    let formatted = format(CalendarEndDate, "EEE. MMM. d, y");
+    setDisplayEndDate(formatted);
+
+    if (eventStartDate === "") {
+      setEventStartDate(CalendarEndDate);
+      let formatted = format(CalendarEndDate, "EEE. MMM. d, y");
+      setDisplayStartDate(formatted);
+    }
+  };
+  // CANCEL BUTTON
+  const cancelStartDate = (event) => {
+    event.preventDefault();
+    document.getElementById("CalendarFormStart").style.visibility = "hidden";
+  };
+  const cancelEndDate = (event) => {
+    event.preventDefault();
+    document.getElementById("CalendarFormEnd").style.visibility = "hidden";
   };
 
-  const handleCancelSubmitDate = (event) => {
-    event.preventDefault();
-    document.getElementById("CalendarForm").style.visibility = "hidden";
-  };
+  /**************************************** */
+  const handleTitle = (value) => setForm({ ...form, title: value });
+  const handleDescription = (value) => setForm({ ...form, description: value });
+  const handleLocation = (value) => setForm({ ...form, location: value });
 
   return (
     <div>
       <div>
-        <button>x</button>
+        <button onClick={() => history.goBack()}>x</button>
         <button>Create event</button>
       </div>
       <form>
         <Top>
-          <Title type="text" placeholder="Your event title" />
-          <Description type="text" placeholder="What will happen?" />
+          <Title
+            type="text"
+            placeholder="Your event title"
+            onChange={(ev) => handleTitle(ev.target.value)}
+          />
+          <Description
+            type="text"
+            placeholder="What will happen?"
+            onChange={(ev) => handleDescription(ev.target.value)}
+          />
         </Top>
         <Section>
           <Label>Date</Label>
@@ -65,7 +140,7 @@ const MeetingForm = () => {
               <SectionInput
                 readOnly
                 placeholder="Start date"
-                onClick={() => handleSelectDateField()}
+                onClick={() => startField()}
                 value={displayStartDate}
               />
               <FiCalendar color="#b3b3b3" />
@@ -74,40 +149,72 @@ const MeetingForm = () => {
             <InputBorder>
               <SectionInput
                 placeholder="End date"
-                onClick={() => handleSelectDateField()}
-                value={displayStartDate}
+                onClick={() => endField()}
+                value={displayEndDate}
               />
               <FiCalendar color="#b3b3b3" />
             </InputBorder>
           </div>
-          <CalendarForm id="CalendarForm">
+
+          <CalendarForm id="CalendarFormStart">
             <Calendar
-              onChange={onCalendarChange}
+              onChange={onStartCalendarChange}
               defaultView="month"
               value={CalendarStartDate}
               prev2Label={null}
               next2Label={null}
-              onClickDay={(value, event) => handleClickDate(value, event)}
+              onClickDay={(value, event) => selectStartDate(value, event)}
             />
             <div className="ButtonBox">
-              <button onClick={(ev) => handleCancelSubmitDate(ev)}>
-                Cancel
-              </button>
-              <button onClick={(event) => handleSubmitDate(event)}>Ok</button>
+              <button onClick={(ev) => cancelStartDate(ev)}>Cancel</button>
+              <button onClick={(event) => submitStartDate(event)}>Ok</button>
+            </div>
+          </CalendarForm>
+
+          <CalendarForm id="CalendarFormEnd">
+            <Calendar
+              onChange={onEndCalendarChange}
+              defaultView="month"
+              value={CalendarEndDate}
+              prev2Label={null}
+              next2Label={null}
+              onClickDay={(value, event) => selectEndDate(value, event)}
+            />
+            <div className="ButtonBox">
+              <button onClick={(ev) => cancelEndDate(ev)}>Cancel</button>
+              <button onClick={(event) => submitEndDate(event)}>Ok</button>
             </div>
           </CalendarForm>
         </Section>
         <Section>
           <Label>Time</Label>
-          <SectionInput type="text" />
-          <div>Icon</div>
+          <TimeRange>
+            <SectionInput3 id="startHour" type="text" placeholder="00" /> :{" "}
+            <SectionInput3 id="startMin" type="text" placeholder="00" />{" "}
+            <SectionInput3 id="startAMPM" type="text" placeholder="PM" />
+            <Arrow>
+              <BsArrowRight />
+            </Arrow>
+            <SectionInput3 id="endHour" type="text" placeholder="00" /> :{" "}
+            <SectionInput3 id="endMin" type="text" placeholder="00" />{" "}
+            <SectionInput3 id="endAMPM" type="text" placeholder="PM" />
+          </TimeRange>
         </Section>
         <Section>
           <Label>Location</Label>
           <div>
-            <SectionInput type="text" />
-            <div>Icon</div>
+            <GrLocation />
+            <SectionInput2
+              type="text"
+              placeholder="Add location"
+              onChange={(ev) => handleLocation(ev.target.value)}
+            />
           </div>
+        </Section>
+        <Section>
+          <Label>Notifications</Label>
+          <RiNotification2Line />{" "}
+          <SectionInput2 type="text" placeholder="Add notification" />
         </Section>
       </form>
     </div>
@@ -184,10 +291,42 @@ const InputBorder = styled.div`
 const SectionInput = styled.input`
   padding: 5px 0;
   font-size: 1rem;
+  width: 140px;
   &:focus {
     outline: none;
   }
-  width: 140px;
+  &::placeholder {
+    opacity: 70%;
+  }
+`;
+
+const SectionInput2 = styled.input`
+  padding: 5px 0;
+  font-size: 1rem;
+  width: 300px;
+  padding-left: 5px;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    opacity: 70%;
+  }
+`;
+
+const SectionInput3 = styled.input`
+  padding: 5px 0;
+  font-size: 1rem;
+  width: 25px;
+  text-align: center;
+  &:focus {
+    outline: none;
+  }
+  &::placeholder {
+    opacity: 70%;
+  }
+  .AMPM {
+    padding: 0 5px;
+  }
 `;
 const CalendarForm = styled.div`
   padding: 0 6vw;
@@ -206,4 +345,13 @@ const CalendarForm = styled.div`
     margin: 10px 0;
   }
 `;
+
+const TimeRange = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Arrow = styled.div`
+  padding: 0 10px;
+`;
+
 export default MeetingForm;
