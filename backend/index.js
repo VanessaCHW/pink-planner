725ab7e01a5b06f48b8d421c1f81850482252d42
Vanessa Chan <chw.vanessa@gmail.com>
2021-03-18 20:03:09 -0400

    Server update

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Backend server is working");
-});
+app
+  .get("/", (req, res) => {
+    res.send("Backend server is working");
+  })
+
+  .post("/newEvent", (req, res) => {
+    res.send("Post!");
+  });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
