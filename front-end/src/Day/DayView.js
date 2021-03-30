72a8ce156563cc5407b6eca19542d4efa8ed2b18
Vanessa Chan <chw.vanessa@gmail.com>
2021-03-29 21:16:46 -0400

    DayView: show info and edit buttons

--- front-end/src/Day/DayView.js
+++ front-end/src/Day/DayView.js
@@ -3,6 +3,9 @@ import { useHistory, useParams } from "react-router";
 import styled from "styled-components";
 import { COLORS } from "../Constants";
 import { format } from "date-fns";
+import { GrLocation } from "react-icons/gr";
+import { BiEditAlt } from "react-icons/bi";
+import { RiDeleteBinLine } from "react-icons/ri";
 
 const dayColors = [
   "rgb(254,182,185)",
@@ -84,11 +87,47 @@ const DayView = () => {
       <ContentSection>
         {dayEvents.map((dayEvent) => (
           <SingleEventBox>
-            <div
-              key={dayEvent.id_}
-              style={{ backgroundColor: dayColors[colorIndex++] }}
-            >
-              {dayEvent.title}
+            <TimeBox>
+              {dayEvent.start.time.allday ? (
+                <div>All day</div>
+              ) : (
+                <>
+                  <div style={{ fontWeight: "500" }}>
+                    {dayEvent.start.time.hours}:{dayEvent.start.time.minutes}{" "}
+                    {dayEvent.start.time.ap}
+                  </div>
+                  <div style={{ fontWeight: "300" }}>
+                    {dayEvent.end.time.hours}:{dayEvent.end.time.minutes}{" "}
+                    {dayEvent.end.time.ap}
+                  </div>
+                </>
+              )}
+            </TimeBox>
+            <div className="RightBox">
+              <EventContentBox
+                style={{ borderLeft: `5px solid ${dayColors[colorIndex++]}` }}
+              >
+                <div className="nameNdesc">
+                  <EventTitle>{dayEvent.title}</EventTitle>
+                  {dayEvent.description ? (
+                    <EventDescr>{dayEvent.description}</EventDescr>
+                  ) : null}
+                </div>
+                {dayEvent.location ? (
+                  <EventLocation>
+                    <GrLocation size="13" /> {dayEvent.location}
+                  </EventLocation>
+                ) : null}
+              </EventContentBox>
+
+              <EventButtonBox>
+                <EventButtons>
+                  <BiEditAlt />
+                </EventButtons>
+                <EventButtons>
+                  <RiDeleteBinLine />
+                </EventButtons>
+              </EventButtonBox>
             </div>
           </SingleEventBox>
         ))}
@@ -97,9 +136,60 @@ const DayView = () => {
   );
 };
 
+const SingleEventBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-bottom: 2px solid #dae2f1;
+  font-weight: 300;
+  padding: 15px 0;
+  .nameNdesc {
+    padding-bottom: 6px;
+  }
+  .RightBox {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
+const TimeBox = styled.div`
+  padding-left: 10px;
+  min-width: 75px;
+  font-size: 0.9rem;
+`;
+const EventContentBox = styled.div`
+  padding-left: 15px;
+  padding-right: 5px;
+`;
+const EventTitle = styled.div`
+  font-size: 1.2rem;
+  font-weight: 400;
+`;
+const EventDescr = styled.div`
+  font-size: 1rem;
+  padding-top: 4px;
+`;
+const EventLocation = styled.div`
+  font-size: 0.95rem;
+`;
+const EventButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-right: 5px;
+`;
+const EventButtons = styled.button`
+  background: transparent;
+  border: none;
+  font-size: 1.3rem;
+  opacity: 30%;
+  display: block;
+  padding: 3px 5px;
+`;
 const Wrapper = styled.div`
   min-height: 100vh;
-  background-color: ${COLORS.background};
+  background: ${COLORS.background};
 `;
 const Header = styled.div`
   position: relative;
@@ -148,11 +238,7 @@ const DateRightSection = styled.div`
 const Path = styled.path`
   fill: url(#gradient);
 `;
-const SingleEventBox = styled.div`
-  border-radius: 5px;
-  background-color: pink;
-  margin: 6px;
-`;
+
 const ContentSection = styled.div`
   position: absolute;
   top: 200px;
