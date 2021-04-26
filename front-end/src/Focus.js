ad1f246d75ebec248379c1afbdb1f46911441710
Vanessa Chan <chw.vanessa@gmail.com>
2021-04-26 18:08:38 -0400

    Focus: correction import paths

--- front-end/src/Focus.js
+++ front-end/src/Focus.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
-import { COLORS } from "../Constants";
-import NewEventDialog from "../Components/NewEventDialog";
+import NewEventDialog from "./Components/NewEventDialog";
 
 const Focus = () => {
   const [task_time, setTaskTime] = useState(25);
